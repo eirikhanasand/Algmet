3cee1deeda4a358adc1dacf8fec6ed0465298cdd
eirikhanasand <eirik.hanasand@gmail.com>
2023-09-07 16:26:05 +0200

    Added tasks 5-7 and fixed indenting of task 1

--- 1.cpp
+++ 1.cpp
@@ -40,6 +40,7 @@
  *
  *   @file     EKS_01_Vector.CPP
  *   @author   Frode Haug, NTNU
+ *   @author   Eirik Hanasand, insert(...), resize(...)
  */
 
 
@@ -56,46 +57,46 @@ using namespace std;
 template <typename T>                //  'template'!  'T': fleksibel datatype!
 class Vector {                       //  (finnes ikke direkte i Java!
                                      //   "Generics" er noe litt lignende.)
-  private:
-      T*  data;                      //  PEKER til array av typen 'T'.
-      int kapasitet, antall;         //  Max.lengde og nåværende antall.
+    private:
+        T*  data;                      //  PEKER til array av typen 'T'.
+        int kapasitet, antall;         //  Max.lengde og nåværende antall.
 
-  public:                      //  Initierer medlemmer vha. default parameter:
-      Vector(const int lengde = 200)  {  data = new T[lengde];
-                                         kapasitet = lengde;  antall = 0;  }
-
-
-      ~Vector()  {  delete [] data;  }  // Frigir allokert memory (vha.'new').
+    public:                      //  Initierer medlemmer vha. default parameter:
+        Vector(const int lengde = 200)  {  data = new T[lengde];
+                                            kapasitet = lengde;  antall = 0;  }
 
 
-      int  capacity()  const  {  return kapasitet;  }
+        ~Vector()  {  delete [] data;  }  // Frigir allokert memory (vha.'new').
 
 
-      void clear()  {                //  Nullstiller arrayens bruk:
-//          for (int i = 0; i < antall; i++)  data[i] = 0;  //  Unødvendig?!!!
-          antall = 0;
-      }
+        int  capacity()  const  {  return kapasitet;  }
 
 
-      void display() const {         //  Skriver HELE Vectorens innhold:
-          for (int i = 0;  i < antall;  i++)
-              cout << i << ": " << data[i] << "  ";
-          cout << "\n\t'antall': " << antall << '\n';
-      }
+        void clear()  {                //  Nullstiller arrayens bruk:
+    //          for (int i = 0; i < antall; i++)  data[i] = 0;  //  Unødvendig?!!!
+            antall = 0;
+        }
 
 
-      bool empty() const  {  return (antall == 0);  }   //  Tomt eller ei.
+        void display() const {         //  Skriver HELE Vectorens innhold:
+            for (int i = 0;  i < antall;  i++)
+                cout << i << ": " << data[i] << "  ";
+            cout << "\n\t'antall': " << antall << '\n';
+        }
 
-                                         //  Tilsvarer:   .... = vec[pos].
-      T get(const int pos) const {       //  Henter (om mulig) element nr.pos:
-          if (pos >= 0  &&  pos < antall)  //  Innenfor bruksområde:
-              return data[pos];            //  Returnerer aktuell verdi.
-          else
-              cout << "\nIndex out of bounds .....\n\n";   //  Egen melding ??
-          return T();
-      }
 
-                                //  Smetter inn (om mulig) 't' på plass 'pos':
+        bool empty() const  {  return (antall == 0);  }   //  Tomt eller ei.
+
+                                            //  Tilsvarer:   .... = vec[pos].
+        T get(const int pos) const {       //  Henter (om mulig) element nr.pos:
+            if (pos >= 0  &&  pos < antall)  //  Innenfor bruksområde:
+                return data[pos];            //  Returnerer aktuell verdi.
+            else
+                cout << "\nIndex out of bounds .....\n\n";   //  Egen melding ??
+            return T();
+        }
+
+                                    //  Smetter inn (om mulig) 't' på plass 'pos':
         bool insert(const int pos, const T t) {
             if (antall < kapasitet)  {        //  OPPGAVE: øke arrayens lengde.
                 if (pos >= 0  &&  pos <= antall) {       //  Lovlig indeks:
@@ -115,82 +116,82 @@ class Vector {                       //  (finnes ikke direkte i Java!
         }
 
 
-      T pop_back() {                      //  Fjerner (om mulig) bakerste:
-          if (!empty()) {                 //  Ikke tomt:
-              T siste = data[antall-1];   //  Tar vare på bakerste.
-              antall--;                   //  Antallet reduseres med -1.
-              return siste;               //  Returnerer bakerste/siste.
-          }  else
-              cout << "\nEmpty Vector - impossible to pop!\n\n";
-          return T();
-      }
+        T pop_back() {                      //  Fjerner (om mulig) bakerste:
+            if (!empty()) {                 //  Ikke tomt:
+                T siste = data[antall-1];   //  Tar vare på bakerste.
+                antall--;                   //  Antallet reduseres med -1.
+                return siste;               //  Returnerer bakerste/siste.
+            }  else
+                cout << "\nEmpty Vector - impossible to pop!\n\n";
+            return T();
+        }
 
 
-      T pop_front() {                     //  Fjerner (om mulig) forreste:
-          if (!empty()) {                 //  Ikke tomt:
-              T forste = data[0];         //  Tar vare på forreste:
-              remove(0);                  //  Fjerner den første.
-              return forste;              //  Returnerer den unnalagrede.
-          } else
-              cout << "\nEmpty Vector - impossible to pop!\n\n";
-          return T();
-      }
+        T pop_front() {                     //  Fjerner (om mulig) forreste:
+            if (!empty()) {                 //  Ikke tomt:
+                T forste = data[0];         //  Tar vare på forreste:
+                remove(0);                  //  Fjerner den første.
+                return forste;              //  Returnerer den unnalagrede.
+            } else
+                cout << "\nEmpty Vector - impossible to pop!\n\n";
+            return T();
+        }
 
 
-      bool push_back(const T t) {         //  Legger ny inn bakerst,
-          return insert(antall, t);       //    vha. annen funksjon.
-      }
+        bool push_back(const T t) {         //  Legger ny inn bakerst,
+            return insert(antall, t);       //    vha. annen funksjon.
+        }
 
 
-      bool push_front(const T t) {        //  Legger ny inn forrest,
-          return  insert(0, t);           //    vha. annen funksjon.
-      }
+        bool push_front(const T t) {        //  Legger ny inn forrest,
+            return  insert(0, t);           //    vha. annen funksjon.
+        }
 
 
-      bool remove(const int pos) {    //  Fjerner (om mulig) element nr.'pos':
-          if (pos >= 0 && pos < antall) {             //  Lovlig indeks:
-              for (int i = pos;  i < antall-1;  i++)  //  Flytter alle ETTERPÅ
-                  data[i] = data[i + 1];              //    ned ETT hakk.
-              antall--;                             //  Totalantallet minskes.
-              return true;
-          }
-          else {
-              cout << "\nIndex out of bounds .....\n\n";   //  Egen melding ??
-              return false;
-          }
-      }
-
-
-        void resize(const int nyLengde, const int val = 0) {   //  Øker (om mulig) arrayens lengde.
-            if (nyLengde > capacity()) {
-                T* temp = new T[nyLengde];
-
-                for (int i = 0; i < capacity(); i++) {
-                    temp[i] = data[i];
-                }
-
-                for (int i = 0; i < nyLengde-capacity(); i++) {
-                    temp[i] = val;
-                }
-
-                delete[] data;
-                data = temp;
-                kapasitet = nyLengde;
-            } else {
-                cout << "The new size must be larger than the current size.\n\n";
+        bool remove(const int pos) {    //  Fjerner (om mulig) element nr.'pos':
+            if (pos >= 0 && pos < antall) {             //  Lovlig indeks:
+                for (int i = pos;  i < antall-1;  i++)  //  Flytter alle ETTERPÅ
+                    data[i] = data[i + 1];              //    ned ETT hakk.
+                antall--;                             //  Totalantallet minskes.
+                return true;
+            }
+            else {
+                cout << "\nIndex out of bounds .....\n\n";   //  Egen melding ??
+                return false;
             }
         }
 
-                                            //  Tilsvarer:   vec[pos] = verdi.
-      void set(const int pos, const T verdi)  {
-          if (pos >= 0  &&  pos < antall)   //  Lovlig indeks:
-              data[pos] = verdi;            //  Setter til ny verdi.
-          else
-              cout << "\nIndex out of bounds .....\n\n";   //  Egen melding ??
-      }
+
+            void resize(const int nyLengde, const int val = 0) {   //  Øker (om mulig) arrayens lengde.
+                if (nyLengde > capacity()) {
+                    T* temp = new T[nyLengde];
+
+                    for (int i = 0; i < capacity(); i++) {
+                        temp[i] = data[i];
+                    }
+
+                    for (int i = 0; i < nyLengde-capacity(); i++) {
+                        temp[i] = val;
+                    }
+
+                    delete[] data;
+                    data = temp;
+                    kapasitet = nyLengde;
+                } else {
+                    cout << "The new size must be larger than the current size.\n\n";
+                }
+            }
+
+                                                //  Tilsvarer:   vec[pos] = verdi.
+        void set(const int pos, const T verdi)  {
+            if (pos >= 0  &&  pos < antall)   //  Lovlig indeks:
+                data[pos] = verdi;            //  Setter til ny verdi.
+            else
+                cout << "\nIndex out of bounds .....\n\n";   //  Egen melding ??
+        }
 
 
-      int  size() const  {  return antall;  }   // Antall nåværende elementer.
+        int  size() const  {  return antall;  }   // Antall nåværende elementer.
 };
 
 
---
+++ 7.cpp
@@ -0,0 +1,60 @@
+/**
+ * Note from Eirik:
+ * Added board as suggested by Frode, made visitgroup function which checks that
+ * a piece has not been visited, if not it starts a new group, prints the
+ * coordinates of the group, the visits the children using recursion of the same
+ * function. Afterwards it prints the total number of members of the group, then
+ * continues until the entire board has been checked. The board is n+2 in size
+ * as this mitigated the need for checking if the position exists as it will
+ * always exist.
+*/
+
+#include <iostream>                  //  cout
+#include <iomanip>                   //  setw
+
+const int SIZE = 10;
+const int PIECE = 1;
+const int SEEN = 2;
+
+int gBoard[SIZE+2][SIZE+2] = {
+    { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+    { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0 },
+    { 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0 },
+    { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+    { 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0 },
+    { 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0 },
+    { 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0 },
+    { 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0 },
+    { 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0 },
+    { 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0 },
+    { 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0 },
+    { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 } 
+};
+
+void visitGroup(int x, int y, int groupNumber, int & members) {
+    if (gBoard[x][y] == PIECE) {
+        gBoard[x][y] = SEEN;
+        members++;
+        std::cout << "x: " << x << "\ty: " << y << "\n";
+        visitGroup(x+1, y, groupNumber, members);
+        visitGroup(x, y+1, groupNumber, members);
+        visitGroup(x-1, y, groupNumber, members);
+        visitGroup(x, y-1, groupNumber, members);
+    }
+}
+
+int main() {
+    int groupNumber = 0, members;
+    
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            if (gBoard[i+1][j+1] == PIECE) {
+                members = 0;
+                std::cout << "Visiting group: " << ++groupNumber << " with members "
+                << "at the following positions: \n";
+                visitGroup(i+1, j+1, groupNumber, members);
+                std::cout << "\n Total: " << members << " members.\n";
+            }
+        }
+    }
+}
